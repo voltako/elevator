075cec1ff751a2cef7874e5027a984f294c1c32e
voltako <54960087+voltako@users.noreply.github.com>
2022-01-19 11:28:17 +0300

    Elevators

---
+++ src/main/java/Building.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Building {
+
+    ArrayList<Elevator> elevators = new ArrayList<>();
+    Elevator elev1 = new Elevator(1,1);
+    Elevator elev2 = new Elevator(2,5);
+    Elevator elev3 = new Elevator(3,9);
+
+
+    public void callElev(){
+        System.out.println("Введите этаж: ");
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextInt()){
+            isAvailableOnFloor(scanner.nextInt());
+        }else {
+            System.out.println("Ошибка! Введите корректный этаж");
+        }
+
+    }
+
+    public void setStatus(){
+        elevators.clear();
+        elevators.add(elev1);
+        elevators.add(elev2);
+        elevators.add(elev3);
+        System.out.println(elevators);
+    }
+
+    public void isAvailableOnFloor(int floor){
+        if((floor == elev1.getFloor())| (floor == elev2.getFloor()) | (floor == elev3.getFloor())){
+            setStatus();
+        }
+        else {
+            if(floor > 0 & floor < 10) {
+                setDistance(floor);
+            }
+            else{
+                System.out.println("Ошибка! Введите корректный этаж");
+            }
+        }
+    }
+
+    public void setDistance(int floor) {
+        int dist1 = Math.abs(floor - elev1.getFloor());
+        int dist2 = Math.abs(floor - elev2.getFloor());
+        int dist3 = Math.abs(floor - elev3.getFloor());
+        if (dist1 < dist2) {
+            if (dist3 < dist1) {
+                elev3.setFloor(floor);
+            } else {
+                elev1.setFloor(floor);
+            }
+            if ((elev1.getFloor() != 1) | (elev3.getFloor() != 1)) {
+                elev2.setFloor(1);
+            } else {
+                elev2.setFloor(floor);
+            }
+            setStatus();
+        }
+        else {
+            if (dist3 < dist2) {
+                elev3.setFloor(floor);
+            }else {
+                elev2.setFloor(floor);
+            }if ((elev2.getFloor() != 1) | (elev3.getFloor() != 1)) {
+                elev1.setFloor(1);
+            }else{
+                elev3.setFloor(1);
+            }
+            setStatus();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Building bulding = new Building();
+        while (true){
+            bulding.callElev();
+        }
+    }
+}
