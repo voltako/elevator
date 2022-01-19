43e3b3d87bc556df5b7f86c39fa17a5fa34f623a
voltako <54960087+voltako@users.noreply.github.com>
2022-01-19 11:27:46 +0300

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Bulding bulding = new Bulding();
-        while (true){
-            bulding.callElev();
-            System.out.println(bulding.elevators);
-        }
-    }
-}
